45b0c4e85781b09861165ed9137a567b1241677b
janproz <53119574+janproz@users.noreply.github.com>
2020-07-20 15:05:02 +0400

    complete complete

--- src/index.js
+++ src/index.js
@@ -1,14 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+const pic = "https://picsum.photos/536/354";
+
 ReactDOM.render(
   <div>
-    <h1>My Favourite Foods</h1>
-    <ul>
-      <li>Bacon</li>
-      <li>Jamon</li>
-      <li>Noodles</li>
-    </ul>
+    <h1 className="heading" contentEditable="true" spellCheck="false">
+      My Favourite Foods
+    </h1>
+    <div>
+      <img
+        src="https://www.thespruceeats.com/thmb/4lbEmBf-I2NUdi4XCTj-1b5cgKE=/960x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/how-to-make-your-own-bacon-4146515-17-5b4643f1c9e77c0037514094.jpg"
+        alt="bacon"
+        className="fav"
+      />
+
+      <img
+        src="https://cafedelites.com/wp-content/uploads/2018/12/Honey-Baked-Ham-GLAZE-IMAGE-4-500x375.jpg"
+        alt="ham"
+        className="fav"
+      />
+
+      <img src={pic + "?grayscale"} alt="ice cream" className="fav" />
+    </div>
   </div>,
   document.getElementById("root")
 );
